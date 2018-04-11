2e9ff5de9042b6c21168a6401a6e6ab783d0be27
lianglunzhong <lianglunzhong@gmail.com>
2018-04-11 10:41:09 +0800

    updated:index

---
+++ src/components/Index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="index">
+    <el-row>
+      <el-col :span="18" :offset="3">
+        <el-button type="default">default</el-button>
+      </el-col>
+      <el-col :span="18" :offset="3">
+        <el-button type="primary">primary</el-button>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data() {
+    return {
+      msg: 'This is app index.'
+    }
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .index {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .el-row {
+      width: 100%;
+      .el-col {
+        margin-top: 10px;
+        margin-bottom: 10px;
+        button {
+          width: 100%;
+        }
+      }
+    }
+  }
+</style>
